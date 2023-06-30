954740782f43d05df6d1cac2520623715d588be6
ddnech <ddnech@gmail.com>
2023-06-30 10:48:25 +0700

    adding migration file

---
+++ projects/server/src/migrations/20230630031315-create-order-item.js
@@ -0,0 +1,45 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Order_items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      orderDetail_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Order_details",
+          key: "id",
+        },
+        onDelete:"CASCADE",
+        onUpdate:"CASCADE",
+      },
+      product_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Products",
+          key: "id",
+        },
+        onDelete:"CASCADE",
+        onUpdate:"CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Order_items');
+  }
+};
