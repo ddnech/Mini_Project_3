4b672b8220949661c6651b29f01d2289d8619b74
ddnech <ddnech@gmail.com>
2023-07-06 10:57:29 +0700

    client side push for modify product

--- projects/client/src/page/modifyProduct.jsx
+++ projects/client/src/page/modifyProduct.jsx
@@ -1,15 +1,49 @@
+import { useEffect, useState } from "react";
 import Footer from "../component/footer"
+import ProductDetails from "../component/oneProduct"
 import EditProduct from "../component/user/editProduct"
 import NavbarDashboard from "../component/user/navbarDashboard"
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+
 
 export default function ModifyProduct() {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  
+  useEffect(() => {
+    getProduct();
+  }, []);
+
+  const getProduct = () => {
+    async function fetchProduct() {
+      try {
+          const response = await axios.get(`http://localhost:8000/api/product/${id}`);
+          setProduct(response.data.data);
+      } catch (error) {
+          console.error('Failed to fetch product:', error);
+      }
+    }
+    fetchProduct();
+  }
+
   return (
   <div className="flex flex-col min-h-screen">
     <div className="sticky top-0 z-50">
       <NavbarDashboard/>
     </div>
     <div>
-        <EditProduct/>
+      <div className="p-4 flex flex-col justify-center items-center">
+          <div className="flex flex-wrap gap-4 md:flex-row md:flex-nowrap">
+            <div className="flex-auto">
+            <ProductDetails product={product} />
+            </div>
+            <div className="flex-auto grid">
+                <EditProduct onSubmit={getProduct} product={product} />
+            </div>
+          </div>
+    </div>
     </div>
     <div className="mt-auto">
       <Footer/>
